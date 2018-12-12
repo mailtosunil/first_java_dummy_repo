94f6c38f9245c96fb0d85e039159053bf041fba3
Sunil <mailtosunilbehera@gmail.com>
2018-12-12 18:00:11 +0530

    added some line

--- src/main/java/com/boot/demo/justgifit/JustGifItApplication.java
+++ src/main/java/com/boot/demo/justgifit/JustGifItApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JustGifItApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello from spring boot");
 		SpringApplication.run(JustGifItApplication.class, args);
 	}
 }
