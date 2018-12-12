c115b1f5ad42b019e3a95df7f154db2aa283e093
Sunil <mailtosunilbehera@gmail.com>
2018-12-12 18:47:10 +0530

    check style changes added

--- src/main/java/com/boot/demo/justgifit/JustGifItApplication.java
+++ src/main/java/com/boot/demo/justgifit/JustGifItApplication.java
@@ -3,10 +3,18 @@ package com.boot.demo.justgifit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author bsunil
+ *
+ */
 @SpringBootApplication
 public class JustGifItApplication {
 
-	public static void main(String[] args) {
+	/**
+	 * to trigger the boot app
+	 */
+	public static void main(final String[] args) {
 		System.out.println("Hello from spring boot");
 		SpringApplication.run(JustGifItApplication.class, args);
 	}
